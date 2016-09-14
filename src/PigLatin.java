93703f875f0a2b0b2e1d0449bbbcc322e3914299
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-14 15:36:14 -0400

    2nd draft

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
 
 public class PigLatin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Welcome message
+		System.out.println("Welcome to the Pig Latin Translator!");
+		
+		System.out.println();
+		
+		StringBuffer str = new StringBuffer();
+	
+		
+		String end = "y";
+		
+		
+		//While loop
+		while (end.equalsIgnoreCase("y")){
+		
+		Scanner trans = new Scanner(System.in);
+		
+		//User prompt
+		System.out.println("Enter a line to be translated: ");
+		String word= trans.nextLine();
+		
+		//Set word to lowercase
+		word = word.toLowerCase();
+		
+		//if -else for replacing constants
+		
+
+        // Determine whether the first character in word is a vowel
+        char firstChar = word.charAt(0); 
+        String vowels = "aeiou";		
+        int vowelIndex = vowels.indexOf(firstChar);
+        
+        
+		}
+        
+        
+        //Ending prompt
+		
 	}
 
 }
+
